0ad59ac3e11f61ca3659473a8634c3c3af493671
Daniel Dai <daeming@gmail.com>
2021-09-17 18:11:57 +0800

    add Flex theme to project

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Daniel Dai'
+SITETITLE = 'Daniel Dai'
 SITENAME = u'Daniel Dai的网络日志'
 SITEURL = ''
 
@@ -12,6 +13,33 @@ TIMEZONE = 'Asia/Chongqing'
 
 DEFAULT_LANG = u'en'
 
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
+
+MAIN_MENU = True
+
+LINKS = (
+    ("Linkedin", "https://www.linkedin.com/in/daniel-dai/"),
+    ("Github", "https://github.com/danieldai"),
+)
+
+SOCIAL = (
+    ("linkedin", "https://www.linkedin.com/in/alexandrevicenzi/en"),
+    ("Github", "https://github.com/danieldai"),
+    ("twitter", "https://twitter.com/alxvicenzi"),
+)
+
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+PYGMENTS_STYLE = "monokai"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -19,7 +47,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-THEME = "/home/daniel/pelican-themes/Flex"
+THEME = "themes/Flex"
 EXTRA_FAVICON = True
 
 # Blogroll
