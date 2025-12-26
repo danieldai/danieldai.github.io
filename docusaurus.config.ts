567f568774123ec53f1cbaa6283078761ae8dcc8
Daniel Dai <daniel@danieldai.com>
2025-12-26 18:22:09 +0800

    fix favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Daniel\'s Blog',
   tagline: 'World is a Matrix',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
